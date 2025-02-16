2fbb4202e29323e4787428d77b5ec184beaff5ee
Gabriel Jesus Gutierrez Ramirez <137426613+gabystdev@users.noreply.github.com>
2025-02-17 00:41:20 +0100

    Update idx-template.nix

--- idx-template.nix
+++ idx-template.nix
@@ -5,7 +5,18 @@
     pkgs.unzip
     pkgs.dart
     pkgs.flutter
+    pkgs.android-sdk
+    pkgs.android-studio
+    pkgs.android-tools
+    pkgs.gradle
   ];
+
+  env = {
+    ANDROID_HOME = "$HOME/android-sdk";
+    ANDROID_SDK_ROOT = "$HOME/android-sdk";
+    ANDROID_AVD_HOME = "$HOME/.android/avd";
+  };
+  
   # Shell script that produces the final environment
   bootstrap = ''
     # Copy the folder containing the `idx-template` files to the final
@@ -19,5 +30,13 @@
     # Remove the template files themselves and any connection to the template's
     # Git repository
     rm -rf "$out/.git" "$out/idx-template".{nix,json}
+
+    echo "⚙️ Instalando dependencias de Flutter..."
+    flutter doctor
+    flutter pub get
+
+    echo "⚙️ Configurando Android SDK y creando AVD..."
+    mkdir -p $ANDROID_HOME
+    sdkmanager "platform-tools" "platforms;android-34" "system-images;android-34;google_apis;x86_64"
   '';
 }
